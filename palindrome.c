0ed7019abf318fef035440d7d262d9c0c8736790
Relex12 <40178652+Relex12@users.noreply.github.com>
2018-10-12 16:01:46 +0200

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -2,11 +2,13 @@
 
 int palindrome (int n, char* mot) {
   if (n == 0 || n == 1) return 1;
-  if (mot[0] != mot[n-1]) return 0;
+  else if (mot[0] != mot[n-1]) return 0;
   else {
     mot = mot+1;      // j'y crois pas trop mais en gros on fait pointer mot vers mot[1]
     return (palindrome((n-2), mot);		// peut etre n-1, mais bon
-  }
+}
+// return (n<2) || mot[0] != mot[n-1] && palindrome(n-2, tab+1);
+	    // l'indentation me casse les b*rnes
 }
 
 int main() {
