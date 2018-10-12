291dde0dfefff9729b7252c1187168167347e35e
Relex12 <40178652+Relex12@users.noreply.github.com>
2018-10-12 15:48:14 +0200

    Supposed to work, palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -2,15 +2,15 @@
 
 int palindrome (int n, char* mot) {
   if (n == 0 || n == 1) return 1;
-  if (mot[0] != mot[n]) return 0;
+  if (mot[0] != mot[n-1]) return 0;
   else {
     mot = mot+1;      // j'y crois pas trop mais en gros on fait pointer mot vers mot[1]
-    return (palindrome((n-2), mot);
+    return (palindrome((n-2), mot);		// peut etre n-1, mais bon
   }
 }
 
 int main() {
-	printf ("Kayak est-il palindrome ? %d\n", palindrome(kayak));
-	printf ("chips est-il palindrome ? %d\n", palindrome(chips));
+	printf ("kayak est-il palindrome ? %d\n", palindrome(5, kayak));
+	printf ("chips est-il palindrome ? %d\n", palindrome(5, chips));
 	return (0);
 }
